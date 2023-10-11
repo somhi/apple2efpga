9b65d0ddd43762a46da3f222a86ee101bcaedb01
somhi <somhix@gmail.com>
2023-10-12 01:19:07 +0200

    gowin update

--- demistify/gowin/spram.v
+++ demistify/gowin/spram.v
@@ -1,1 +1,21 @@
-/home/jordi/FPGAs/FPGA-Deca/Deca_17/_A_GITHUB/Apple2efpga_dmty_cape/Apple2efpga_dmty_3p_cape/demistify/xilinx/spram.v
+module spram
+#
+(
+	parameter addrbits = 9,
+	parameter databits = 8,
+	parameter init_file = ""
+)
+(
+	input  wire [addrbits-1:0] address,
+	input  wire clock,
+	input  wire [databits-1:0] data,	//kept for module interchangeability
+	input  wire wren,
+	output reg  [databits-1:0] q
+);
+
+reg[databits-1:0] ram[(2**addrbits)-1:0];
+initial $readmemh(init_file, ram, 0);
+
+always @(posedge clock) if(~wren) q <= ram[address];
+
+endmodule
